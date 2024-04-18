98b3122a5b6682959a0eb2c21cc55fe6c23d62b7
anne <1005593730@qq.com>
2024-04-18 17:24:26 +0800

    add perf test and new example

--- example/01_bypass_net.py
+++ example/01_bypass_net.py
@@ -1,6 +1,6 @@
 import __init__
 import numpy as np
-
+import time
 from paiboard import PAIBoard_SIM
 # from paiboard import PAIBoard_PCIe
 # from paiboard import PAIBoard_Ethernet
@@ -17,8 +17,13 @@ if __name__ == "__main__":
     test_num = 1
     for i in range(test_num):
         input_spike = np.eye(timestep, dtype=np.int8)
+
+        t1 = time.time()
         output_spike = snn(input_spike, TimeMeasure=False)
+        t2 = time.time()
+        
+        snn.record_time(t2 - t1)
         assert np.equal(input_spike, output_spike).all()
     print("Test passed!")
-
+    snn.paicore_status()
     snn.perf(test_num)
